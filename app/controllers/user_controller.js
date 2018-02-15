c5d621f75163d5910d3ce956bd846403a042dad4
Rajiv Ramaiah <rajivramaiah@me.com>
2018-02-14 23:44:57 -0500

    changes

--- app/controllers/user_controller.js
+++ app/controllers/user_controller.js
@@ -1,4 +1,10 @@
-// Rajiv Ramaiah , CoinDuel 2018
+/*
+ * user_controller.js
+ *
+ * User endpoints interactions
+ * 14 Feb 2018
+ * Rajiv Ramaiah
+ */
 
 import User from '../models/user.js';
 
@@ -12,7 +18,6 @@ export const signup = (req, res, next) => {
   if (!email || !password || !username) {
     return res.status(422).send('You must provide an email, a password, and a username to sign up!');
   }
-
   // add query to check if user exists
   User.findOne({ "username": username })
   .then((user) => {
@@ -43,7 +48,7 @@ export const signup = (req, res, next) => {
 export const findUser = (req, res) => {
   console.log("checking for user");
   console.log(req.body);
-  // add query to check if user exists
+  // find user
   User.findOne({ "username": req.body.username })
   .then((user) => {
       if (user) {
@@ -59,7 +64,7 @@ export const findUser = (req, res) => {
   });
 };
 
-// check if user exists
+// return a list of all users in the app
 export const getAllUsers = (req, res) => {
   console.log("getting all users");
   // add query to check if user exists
@@ -78,6 +83,7 @@ export const getAllUsers = (req, res) => {
   });
 };
 
+// delete a user, used for account deletion
 export const deleteUser = (req, res) => {
   console.log('DELETE USER');
   User.remove({ "username": req.body.username })
@@ -87,11 +93,4 @@ export const deleteUser = (req, res) => {
    .catch(error => {
      res.json({ error });
    });
-};
-
-
-
-export const signin = (req, res, next) => {
-  console.log('sign in started');
-  res.send({ token: tokenForUser(req.user) });
 };
