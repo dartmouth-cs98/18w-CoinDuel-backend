b79efb5538ac972feb73783cbc09b71bdf4181f8
Joshua Kerber <jkerber14@comcast.net>
2018-02-14 19:36:27 -0500

    price controller -> coin controller

--- app/controllers/coin_controller.js
+++ app/controllers/coin_controller.js
@@ -1,7 +1,7 @@
 /*
- * price_controller.js
+ * coin_controller.js
  *
- * Cryptocurrency price operations.
+ * Cryptocurrency coin operations via CoinMarketCap
  * 14 Feb 2018
  * Josh Kerber
  */
@@ -9,13 +9,12 @@
 const getJSON = require('get-json');
 
 /*
- * Get the current price of a cryptocurrency.
- * @param req, ex. {symbol: 'BTC'}
- * @param res, ex. {price: 123.45}
+ * Get the current data about a cryptocurrency.
+ * @param req, ex. {symbol: 'ETH'}
  */
-export const getPrice = (req, res) => {
+export const getCoin = (req, res) => {
   const symbol = req.body.symbol;
-  const tickers = req.app.locals.tickers;
+  const tickers = req.app.locals.tickers;   // global ticker dict
 
   // check if request has ticker
   if (!symbol) {
@@ -29,15 +28,15 @@ export const getPrice = (req, res) => {
     res.status(422).send('Coin with ticker \'' + symbol + '\' not found.');
   } else {
 
-    // get coin price from CoinMarketCap
+    // get coin data from CoinMarketCap
     var priceReq = getJSON('https://api.coinmarketcap.com/v1/ticker/' + coinId + '/', (err, crypto) => {
       if (err) {
         res.status(422).send('Unable to retrieve price - please check http://api.coinmarketcap.com/. ERROR: ' + err);
         return;
       }
 
-      // send back price
-      res.send({'price': crypto[0].price_usd});
+      // send back data as object
+      res.send(crypto[0]);
     });
   }
 };
