f223996797c6eec0cba7ed7e7ddfddd8405ef315
Henry Wilson <henryhwilson@gmail.com>
2018-02-18 09:37:20 -0500

    added different coin balances for test data

--- test_data.js
+++ test_data.js
@@ -10,9 +10,9 @@ db.getCollectionNames().forEach(
 
 db.users.insertMany([
 	{ _id: new ObjectId("5a8607d4971c50fbf29726a2"), email: '1@gmail.com', username: 'p1', password: '1', coinBalance: 0 },
-	{ _id: new ObjectId("5a8607d5971c50fbf29726a3"), email: '2@gmail.com', username: 'p2', password: '2', coinBalance: 0 },
-	{ _id: new ObjectId("5a8607d6971c50fbf29726a4"), email: '3@gmail.com', username: 'p3', password: '3', coinBalance: 0 },
-	{ _id: new ObjectId("5a8607d6971c50fbf29726a5"), email: '4@gmail.com', username: 'p4', password: '4', coinBalance: 0 }])
+	{ _id: new ObjectId("5a8607d5971c50fbf29726a3"), email: '2@gmail.com', username: 'p2', password: '2', coinBalance: 10 },
+	{ _id: new ObjectId("5a8607d6971c50fbf29726a4"), email: '3@gmail.com', username: 'p3', password: '3', coinBalance: 30 },
+	{ _id: new ObjectId("5a8607d6971c50fbf29726a5"), email: '4@gmail.com', username: 'p4', password: '4', coinBalance: 40 }])
 
 db.games.insertMany([
 	{ _id: new ObjectId("5a8608233d378876bf62d817"), start_date: new Date(1519048800000), finish_date: new Date(1519423200000), currency_list: ["BTC", "ETH", "XRP", "BCH", "LTC"]},
