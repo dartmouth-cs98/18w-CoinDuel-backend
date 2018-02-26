f650fcd422460941cca910d081a0e235ea7974ec
Joshua Kerber <jkerber14@comcast.net>
2018-02-26 01:03:39 -0500

    updated test

--- scheduledJobs/testJob.py
+++ scheduledJobs/testJob.py
@@ -2,12 +2,12 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon', hour=1)
-def scheduled_job():
+@sched.scheduled_job('cron', day_of_week='mon', hour=1, minute=5)
+def scheduled_job1():
     print('Testing a job that starts on Mondays at 1am.')
 
-@sched.scheduled_job('cron', day_of_week='mon', hour=6)
-def scheduled_job():
+@sched.scheduled_job('cron', day_of_week='mon', hour=6, minute=5)
+def scheduled_job2():
     print('Testing a job that starts on Mondays at 6am UTC.')
 
 sched.start()
