c6a1dd5b5a0d70874cd9f7f1092b72a9de30e862
Joshua Kerber <jkerber14@comcast.net>
2018-03-13 04:00:41 -0400

    encrypted passwords

--- app/controllers/user_controller.js
+++ app/controllers/user_controller.js
@@ -7,39 +7,44 @@
  */
 
 import User from '../models/user.js';
+import dotenv from 'dotenv';
+import Cryptr from 'cryptr';
+
+// password decryption
+dotenv.config({ silent: true });
+const cryptr = new Cryptr(process.env.API_SECRET);
 
 export const signup = (req, res, next) => {
-  console.log('sign up started');
+  console.log('Signing up user...');
+  console.log(req.body);
   const email = req.body.email;
   const password = req.body.password;
   const username = req.body.username;
-  console.log(req.body);
 
   if (!email || !password || !username) {
     return res.status(422).send('You must provide an email, a password, and a username to sign up!');
   }
+
   // add query to check if user exists
   User.findOne({ "username": username })
   .then((user) => {
       if (user) {
         return res.status(422).send('The password or email or username you entered has been taken!');
-      }
-      else {
+      } else {
         const newUser = new User();
         newUser.email = email;
         newUser.password = password;
         newUser.username = username;
         newUser.coinBalance = 30;
         newUser.save()
-          .then((result) => {
-            res.send(newUser);
-          })
-          .catch(err => {
-            res.send(`${err}`);
-          });
+        .then((result) => {
+          res.send(newUser);
+        })
+        .catch(err => {
+          res.send(`${err}`);
+        });
       }
-    }
-  )
+  })
   .catch(err => {
     res.status(400).send(`${err}`);
   });
@@ -47,51 +52,44 @@ export const signup = (req, res, next) => {
 
 // check if user exists
 export const findUser = (req, res) => {
-  console.log("checking for user");
+  console.log('Signing in user...');
   console.log(req.body);
-  // find user
   User.findOne({ "username": req.body.username })
   .then((user) => {
       if (user) {
+
+        // decrypt password
+        user.password = cryptr.decrypt(user.password);
         res.send(user);
-      }
-      else {
+      } else {
         return res.status(422).send('No user found!');
       }
-    }
-  )
-  .catch(err => {
+  }).catch(err => {
     res.status(400).send(`${err}`);
   });
 };
 
 // return a list of all users in the app
 export const getAllUsers = (req, res) => {
-  console.log("getting all users");
   // add query to check if user exists
   User.find()
   .then((users) => {
       if (users) {
         res.send(users);
-      }
-      else {
+      } else {
         return res.status(422).send('No users found!');
       }
-    }
-  )
-  .catch(err => {
+  }).catch(err => {
     res.status(400).send(`${err}`);
   });
 };
 
 // delete a user, used for account deletion
 export const deleteUser = (req, res) => {
-  console.log('DELETE USER');
   User.remove({ "username": req.body.username })
    .then(() => {
      res.json({ message: 'Usage successfully deleted!' });
-   })
-   .catch(error => {
+   }).catch(error => {
      res.json({ error });
    });
 };
--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,12 @@
 // From http://cs52.me/assignments/sa/server-side/
 
 import mongoose, { Schema } from 'mongoose';
+import dotenv from 'dotenv';
+import Cryptr from 'cryptr';
+
+// password encryption
+dotenv.config({ silent: true });
+const cryptr = new Cryptr(process.env.API_SECRET);
 
 const UserSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
@@ -17,7 +23,21 @@ UserSchema.set('toJSON', {
 //   return this.coinBalance;
 // });
 
+// encrypt passwords before saving
+// based on http://cs52.me/assignments/hw5p2/
+UserSchema.pre('save', function beforeyYourModelSave(callback) {
+  const user = this;
 
+  // don't hash preexisting passwords
+  if (!user.isModified('password')) return callback();
+
+  // generate encrypted password
+  var hash = cryptr.encrypt(user.password);
+  console.log(hash);
+  user.password = hash;
+  console.log(user);
+  return callback();
+});
 
 // create model class
 const UserModel = mongoose.model('User', UserSchema);
