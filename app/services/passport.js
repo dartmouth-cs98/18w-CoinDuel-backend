76db93717b539cb1d409e48aa03e9f385966ce51
Kooshul Jhaveri <kooshul@gmail.com>
2018-04-16 17:30:01 -0400

    Using guide from CS52 to continue encryption

--- app/services/passport.js
+++ app/services/passport.js
@@ -9,3 +9,25 @@ const jwtOptions = {
   jwtFromRequest: ExtractJwt.fromHeader('authorization'),
   secretOrKey: process.env.AUTH_SECRET,
 };
+
+const localLogin = new LocalStrategy(localOptions, (username, password, done) => {
+  // Verify this email and password, call done with the user
+  // if it is the correct email and password
+  // otherwise, call done with false
+  User.findOne({ username }, (err, user) => {
+    if (err) { return done(err); }
+
+    if (!user) { return done(null, false); }
+
+    // compare passwords - is `password` equal to user.password?
+    user.comparePassword(password, (err, isMatch) => {
+      if (err) {
+        done(err);
+      } else if (!isMatch) {
+        done(null, false);
+      } else {
+        done(null, user);
+      }
+    });
+  });
+});
