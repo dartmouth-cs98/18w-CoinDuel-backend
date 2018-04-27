e34f94f6d233b51f25526c52a0d79e698bb8c474
Kooshul Jhaveri <kooshul@gmail.com>
2018-04-26 23:10:44 -0400

    further debugging

--- app/controllers/user_controller.js
+++ app/controllers/user_controller.js
@@ -60,8 +60,7 @@ export const signup = (req, res, next) => {
         newUser.save()
         .then(result => {
           var email_html = req.app.locals.resources.mailgun_email1 + username + req.app.locals.resources.mailgun_email2 + verificationId + req.app.locals.resources.mailgun_email3;
-          console.log(__dirname);
-          var filepath = path.join(__dirname, '../images/logo.png');
+          var filepath = path.join('../images/logo.png');
           console.log(filepath);
           var data = {
             from: 'CoinDuel Mailer <noreply@coinduel.co>',
