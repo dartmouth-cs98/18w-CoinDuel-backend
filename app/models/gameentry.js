928b3a9c701edc60be8d0a770591d2fa0ab12d03
Joshua Kerber <jkerber14@comcast.net>
2018-05-10 22:59:35 -0400

    initial entry balance

--- app/models/gameentry.js
+++ app/models/gameentry.js
@@ -12,7 +12,7 @@ const GameEntrySchema = new Schema({
     gameId: { type: Schema.Types.ObjectId, ref: 'Game' },
     userId: { type: Schema.Types.ObjectId, ref: 'User' },
     currentChoices: [{ symbol: { type: String }, allocation: { type: Number, min: 0, max: 10 }}],
-    coin_balance: { type: Number, default: 0 },
+    coin_balance: { type: Number, default: 10 },
     last_updated: { type: Date, default: Date.now },
     game_performance: [{ time: { type: Number, default: 0 }, unixTime: { type: Number, default: Date.now }, capCoin: {type: Number, default: 0 } }]
 });
