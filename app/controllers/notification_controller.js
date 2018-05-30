e18dfeb67ab1073ed5b5e99b04328bf53a7683e5
Kooshul Jhaveri <kooshul@gmail.com>
2018-05-30 16:50:15 -0400

    Fixed weird commit revert for notifs pt. 4

--- app/controllers/notification_controller.js
+++ app/controllers/notification_controller.js
@@ -51,7 +51,7 @@ export const preGameNotify = (req, res) => {
       $lte: start_date,
     }
   })
-  .sort({'start_date': -1})
+  .sort({start_date: -1})
   .limit(1)
   .then((result) => {
     // if such a game exists, schedule a pre-game notification through OneSignal
@@ -113,7 +113,7 @@ export const postGameNotify = (req, res) => {
       $lte: finish_date,
     }
   })
-  .sort({'finish_date': -1})
+  .sort({finish_date: -1})
   .limit(1)
   .then((result) => {
     // if such a game exists, schedule a post-game notification through OneSignal
